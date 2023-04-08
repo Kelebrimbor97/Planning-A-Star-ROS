067e435211eca41d89ddccfcd4da127e22180f6e
Nishad Kulkarni <35636842+Kelebrimbor97@users.noreply.github.com>
2023-04-08 02:59:40 -0400

    Helper code

---
+++ Cost.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def cost(Xi,Yi,Thetai,UL,UR):
+    t = 0
+    r = 0.038
+    L = 0.354
+    dt = 0.1
+    Xn=Xi
+    Yn=Yi
+    Thetan = 3.14 * Thetai / 180
+    x_set = []
+    y_set = []
+
+
+# Xi, Yi,Thetai: Input point's coordinates
+# Xs, Ys: Start point coordinates for plot function
+# Xn, Yn, Thetan: End point coordintes
+    D=0
+    while t<1:
+        t = t + dt
+        # Xs = Xn
+        # Ys = Yn
+        x_set.append(Xn)
+        y_set.append(Yn)
+        Xn += 0.5*r * (UL + UR) * math.cos(Thetan) * dt
+        Yn += 0.5*r * (UL + UR) * math.sin(Thetan) * dt
+        Thetan += (r / L) * (UR - UL) * dt
+        D=D+ math.sqrt(math.pow((0.5*r * (UL + UR) * math.cos(Thetan) * dt),2)+math.pow((0.5*r * (UL + UR) * math.sin(Thetan) * dt),2))
+    Thetan = 180 * (Thetan) / 3.14
+    return Xn, Yn, Thetan, D # x_set, y_set
+    
+
+# actions=[[5,5], [10,10],[5,0],[0,5],[5,10],[10,5]]
+        
+# for action in actions:
+#      k=cost(0,0,45, action[0],action[1])      # (0,0,45) hypothetical start configuration, this dosn't matter for calucating the edges'costs
+#      print(k[3])
+  
+ 
+
+    
